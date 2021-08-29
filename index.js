1ab6c8dc6a551ba9cafa7f072bb4b5de1968bdad
Paolo Rechia <paolorechia@gmail.com>
2021-08-29 08:02:57 -0300

    Black magic

--- index.js
+++ index.js
@@ -3,25 +3,86 @@ const cache = require('@actions/cache');
 const crypto = require('crypto');
 const fs = require('fs');
 
-
-const wait = require('./wait');
 const { spawn } = require('child_process');
 
-run();
+let python_path = undefined
 
-const py_get_site_packages = `\
-import sys \
-print([path for path in sys.path if "site-packages" in path][0])
-`
+function register_proc_handlers(proc, next_step) {
 
-spawn_list = [
- ('python3', ['-m', 'pip', 'install', 'pytest']),
- ('python3', ['-c', py_get_site_packages]) 
-]
+    proc.stdout.on('data', (data) => {
+          console.log(`stdout: ${data}`);
+    });
+    proc.stderr.on('data', (data) => {
+          console.error(`stderr: ${data}`);
+    });
+    proc.on('close', next_step)
+}
+
+async function base_step(step_data) {
+    console.log('On base step', step_data)
+    if (step_data.code === 0) {
+        try {
+          if (step_data.spawn_hook) {
+              proc = step_data.spawn_hook() 
+              register_proc_handlers(proc, step_data.next_step)
+          }
+          if (step_data.next_step && !step_data.spawn_hook) {
+              await step_data.next_step(0)
+          }
+        } catch (error) {
+          core.setFailed(error.message);
+        }
+    } else {
+        core.setFailed("Step failed");
+    }
+}
+
+async function install_pytest(code) {
+    console.log('Installing pytest...')
+    await base_step(
+        {
+            name: 'install pytest',
+            code,
+            spawn_hook: function() {
+                return spawn('python3', ['-m', 'pip', 'install', 'pytest'])
+            },
+            next_step: finish_pytest
+        }
+    )
+}
+
+async function finish_pytest(code) {
+    console.log('Installed pytest.')
+    await base_step({
+        name: 'finish pytest',
+        code,
+        next_step: get_packages
+    })
+}
+
+
+async function get_packages(code) {
+    await base_step(
+        {
+            code,
+            spawn_hook: function() {
+                const proc = spawn('python3', ['-c', 'import sys; import json; print(json.dumps(sys.path))']) 
+                proc.stdout.on('data', function(data) { python_path = JSON.parse(data) } )
+                return proc
+            },
+            next_step: finish_get_site_packages
+        }
+    )
+}
+
+async function finish_get_site_packages(code) {
+    console.log('Found pathes: ', python_path)
+}
 
 async function run() {
   try {
     console.log('Starting...')
+    await install_pytest(0)
   } catch (error) {
     core.setFailed(error.message);
   }
@@ -35,4 +96,6 @@ function hash_file(filename) {
     const hex = hashSum.digest('hex');
     return hex
 }
+
+run();
 
